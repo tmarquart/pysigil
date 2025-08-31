6c9859e1ecfdcdb458fb142c175b9d899c0b87c8
Tim Marquart <tmarquart@gmail.com>
2025-08-30 19:21:09 -0500

    Manual test tweaks

--- tests/manual/manual_ui.py
+++ tests/manual/manual_ui.py
@@ -6,7 +6,14 @@ registered providers and allow selecting one to display its fields.
 
 from pysigil.ui.tk import App
 
+from pysigil import api
+
+
 if __name__ == "__main__":  # pragma: no cover - manual test only
+    prov=api.handle('sigil-dummy')
+    #prov.add_field("api_field", "string")
+    prov.set("api_field", "42", scope="environment")  # sets SIGIL_PKG_API_FIELD
+
     app = App()
     app.root.mainloop()
 
